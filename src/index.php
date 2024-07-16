c14651bc75edf4d6a9b50aeedd8a8ea4e2920ebc
adrian <adrian.adriano.tech@gmail.com>
2024-07-16 13:29:10 -0400

    fix(index): remove sleep commands, does not work well with container

--- src/index.php
+++ src/index.php
@@ -3,23 +3,13 @@ try {
     // Control variable
     $showPhpInfo = false; // Set to false to display another message
 
-    // Function to keep the script running
-    function keepAlive() {
-        while (true) {
-            echo "Container is running...\n";
-            sleep(10);
-        }
-    }
-
     // Display PHP info or a custom message based on the control variable
     if ($showPhpInfo) {
         phpinfo();
     } else {
         echo "PHP Info successfully hidden.\n";
     }
-
-    // Keep the container alive
-    keepAlive();
+    
 } catch (Exception $e) {
     // Handle any exceptions
     echo "An error occurred: " . $e->getMessage();
