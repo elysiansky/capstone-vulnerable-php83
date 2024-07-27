ce3f04894f49f40c20c47c7045532ffe64972f0e
NadaKamel03 <nadakamel2003@gmail.com>
2024-07-27 16:34:27 -0400

    (patch): ICMP Patch

--- docker/php83/entrypoint.sh
+++ docker/php83/entrypoint.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# Read the PODSLEEP environment variable (default to "no" if not set)
+# Read environment variables with default values
 PODSLEEP=${PODSLEEP:-no}
+DISABLE_ICMP=${DISABLE_ICMP:-no}
 
 # Check the value of PODSLEEP and act accordingly
 if [ "$PODSLEEP" = "yes" ]; then
   echo "[ENTRYPOINT] PODSLEEP is set to yes. Sleeping the container for 9999 days..."
   sleep 9999d
 else
+  # Disable ICMP if specified
+  if [ "$DISABLE_ICMP" = "true" ]; then
+      echo "Disabling ICMP..."
+      sysctl -w net.ipv4.icmp_echo_ignore_all=1
+  else
+      echo "ICMP remains enabled."
+  fi
+  
+  # Execute the main container command and replace the shell process with php-fpm
   echo "[ENTRYPOINT] PODSLEEP is set to no. Running PHP-FPM..."
-  php-fpm
+  exec php-fpm
 fi
+
+# Print environment variables for debugging
+printenv | sort
